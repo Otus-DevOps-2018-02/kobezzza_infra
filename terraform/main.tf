ec0694feb1979aa3c33b27eeadfd5fb76515df18
kobezzza <kobezzza@gmail.com>
2018-04-01 15:22:38 +0300

    Added multiple nodes; Added project ssh keys

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,21 @@ provider "google" {
   region  = "${var.region}"
 }
 
+#resource "google_compute_project_metadata_item" "default" {
+#  key = "ssh-keys"
+#  value = "appuser:${file(var.public_key_path)}"
+#}
+
+resource "google_compute_project_metadata_item" "default" {
+  key = "ssh-keys"
+  value = "appuser1:${file(var.public_key_path)} appuser2:${file(var.public_key_path)}"
+}
+
 resource "google_compute_instance" "app" {
-  name         = "reddit-app"
+  name         = "reddit-app-${count.index}"
   machine_type = "g1-small"
   zone         = "${var.zone}"
+  count        = "${var.vm_count}"
 
   boot_disk {
     initialize_params {
