629c1df121537405bdcaaf954dedd2e93aba7820
kobezzza <kobezzza@gmail.com>
2018-04-01 15:53:37 +0300

    Added load balancer

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "app_external_ip_1" {
 output "app_external_ip_2" {
   value = "${google_compute_instance.app.*.network_interface.0.access_config.0.assigned_nat_ip[1]}"
 }
+
+output "app_external_ip_on_lb" {
+  value = "${google_compute_global_forwarding_rule.reddit_app_forwarding_rule.ip_address}"
+}
